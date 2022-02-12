44bc58016f9f739d8b68f7daf0401c6cd2188c15
Joe Carboni <jcarboni@shupecarboni.com>
2022-02-12 17:05:43 -0500

    added robots file

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory
 from application.routes import adp
 
 
@@ -6,5 +6,9 @@ def create_app():
 
     app = Flask(__name__)
     app.register_blueprint(adp)
+
+    @app.route("/robots.txt")
+    def robots():
+        return send_from_directory(app.static_folder, request.path[1:])
 
     return app
