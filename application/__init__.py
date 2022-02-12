89a19dfe10a81fe65a661d74506df2035a4a81ad
Joe Carboni <jcarboni@shupecarboni.com>
2022-02-11 21:34:34 -0500

    first commit

---
+++ application/__init__.py
@@ -0,0 +1,106 @@
+import os
+import pandas
+from hashlib import sha256
+from flask import Flask, Response, request
+from io import BytesIO
+from application import adp_retrieval, emailHelper
+from sqlalchemy import create_engine
+
+
+DATABASE = os.environ.get('DATABASE_URL')
+TABLE = 'users'
+
+def create_app():
+
+    app = Flask(__name__)
+
+    @app.route('/adp-reports')
+    def request_report():
+        key = request.args.get('api-key')
+        if not key:
+            return Response("Enter your api key into the url set to the parameter 'api-key'",200)
+        engine = create_engine(DATABASE)
+        with engine.connect() as conn:
+            db_tables = conn.execute("""
+                SELECT table_name 
+                FROM information_schema.tables 
+                WHERE table_schema = 'public' 
+                ORDER BY table_name;
+            """).fetchall()
+            db_tables_namelist = [tab[0] for tab in db_tables]
+            if TABLE not in db_tables_namelist:
+                return Response("No users have been initialized. Register the first user.", 500)
+            else:
+                user = conn.execute(f"SELECT * FROM {TABLE} WHERE api_key = %s;", [key]).fetchone()
+                if user:
+                    try:
+                        report_bytes: bytes = adp_retrieval.fetch_data(
+                            company=user[0],
+                            user=user[1],
+                            password=user[2]
+                        )
+                    except adp_retrieval.LoginError:
+                        emailHelper.send_email(
+                            user[3],
+                            "Failed to Login",
+                            "Failed to log-in to ADPnow.com with your credentials",
+                            "",""
+                        )
+                        return Response(None,200)
+                    else:
+                        emailHelper.send_email(
+                            user[3],
+                            "ADP Open Order & Shipments",
+                            "","","",
+                            (report_bytes, "ADP Report.xls")
+                        )
+                        return Response(None,200)
+                else:
+                    return Response("user not found", 200)
+
+
+    @app.route('/adp-reports/new_user')
+    def register_user():
+        company = request.args.get('company', type=str)
+        user = request.args.get('user', type=str)
+        password = request.args.get('password', type=str)
+        email = request.args.get('email', type=str)
+
+        engine = create_engine(DATABASE)
+        with engine.connect() as conn:
+            api_key = sha256((company+user+email+password).encode()).hexdigest()
+            db_tables = conn.execute("""
+                SELECT table_name 
+                FROM information_schema.tables 
+                WHERE table_schema = 'public' 
+                ORDER BY table_name;
+            """).fetchall()
+            db_tables_namelist = [tab[0] for tab in db_tables]
+            if TABLE not in db_tables_namelist:
+                first_user = pandas.DataFrame(
+                    {'company': [company],
+                    'username': [user],
+                    'password': [password],
+                    'email_address': [email],
+                    'api_key': [api_key]}
+                    )
+                first_user.to_sql(TABLE, conn, index=False)
+                emailHelper.send_email(email,"Resgistration Complete",msg,"","")
+                return Response("Database initialized and first user created", 200)
+            else:
+                conn.execute(
+                    f"""
+                    INSERT INTO {TABLE} (company, username, password, email_address, api_key) 
+                    VALUES(%s, %s, %s, %s, %s);
+                    """,
+                    (company, user, password, email, api_key))
+                msg = f"""
+                <p>Successfully Registered.</p>
+                <p>Use this key for requests to have the reports automatically emailed to you upon request</p>
+                <p style="margin-left: 15px">{api_key}</p>
+                <p>You would use the key like this --> https://adpreports-heroku.com/adp-reports?api_key=<b><u>YOUR-KEY</u></b>
+                """
+                emailHelper.send_email(email,"Resgistration Complete",msg,"","")
+                return Response("successfully registered",200)
+
+    return app
---
+++ application/adp_retrieval.py
@@ -0,0 +1,83 @@
+import requests
+import json
+import os
+from bs4 import BeautifulSoup
+
+_HERE = os.path.dirname(__file__)
+
+with open(os.path.join(_HERE,'./config.json'),'r') as config_file:
+    CONFIGS = json.load(config_file)
+
+URL_LOGIN, URL_A, URL_B, URL_C = CONFIGS["URLS"].values()
+
+class LoginError(Exception):
+    pass
+
+def fetch_data(**kwargs) -> bytes:
+    company = kwargs['company']
+    user = kwargs['user']
+    password = kwargs['password']
+    
+    with requests.Session() as session:
+
+        session.headers.update(
+            {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0"}
+        )
+
+        payload_login = {
+            "login_submit": "on",
+            "login_do_redirect": "1",
+            "no_cert_storing": "on",
+            "lennox_company": f"{company}",
+            "j_user": f"{company}.{user}",
+            "j_authscheme": "default",
+            "lennox_uid": f"{user}",
+            "j_password": f"{password}",
+            "saveid": "on",
+            "j_authscheme": "ADP_Auth"
+        }
+
+        payload_a = {
+            "NavigationTarget": "navurl://981bf7b1999315eef10dc916d4220d96",
+            "LightDTNKnobID": "1705057238",
+            "ClientWindowID": "WID1640133263081",
+            "$Roundtrip": "true",
+            "$DebugAction": "null"
+            }
+
+        # get logged in
+        resp = session.post(URL_LOGIN, data=payload_login)
+        if BeautifulSoup(resp.text, "html.parser").find("span",string="User authentication failed"):
+            raise LoginError("Authentication failed. Check credentials and try again")
+
+        # get page with body values for next request
+        resp = session.post(URL_A, data=payload_a)
+
+        # find values and build next payload
+        soup = BeautifulSoup(resp.text, "html.parser")
+        form = soup.find("form",{"id": "form_Redirect_9223372035027316663"}).find_all("input")
+        payload_b = {child['name']:child['value'] for child in form}
+
+        # send request with new payload
+        resp = session.post(URL_B, data=payload_b)
+
+        # find values and build next payload
+        soup = BeautifulSoup(resp.text, "html.parser")
+        names_to_find =["sap-ext-sid","sap-wd-cltwndid","sap-wd-norefresh","sap-wd-secure-id"]
+        payload_c = {name:soup.find("input", {"name":name})["value"] for name in names_to_find}
+        payload_c.update({"SAPEVENTQUEUE":"Button_Press\ue002Id\ue004aaaa.SalesmanReportCompView.btnGenerateExcel\ue003\ue002ClientAction\ue004submit\ue003\ue002urEventName\ue004BUTTONCLICK\ue003\ue001Form_Request\ue002Id\ue004...form\ue005Async\ue004false\ue005FocusInfo\ue004@{\"sFocussedId\": \"aaaa.SalesmanReportCompView.btnGenerateExcel\"}\ue005Hash\ue004\ue005DomChanged\ue004false\ue005IsDirty\ue004false\ue003\ue002EnqueueCardinality\ue004single\ue003\ue002\ue003"})
+
+        # send request with new payload
+        resp = session.post(URL_C, data=payload_c)
+
+        # remove unusual tags so BS can parse the html
+        cleaned = resp.text.replace("<![CDATA[","").replace("]]>","").replace("<initialize-ids>","") \
+            .replace("</initialize-ids>","").replace("<script-call>","").replace("<\script-call>","")
+
+        # get final download link
+        download_link = BeautifulSoup(cleaned,'html.parser').find('a')['href']
+        
+        # get data
+        resp = session.get(download_link.replace("../../", "https://adpinside.com/webdynpro/resources/"))
+
+        return resp.content
